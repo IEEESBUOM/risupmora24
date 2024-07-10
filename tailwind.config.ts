bb54dce479e1af22556d6713649758df3324056c
MADHUSHANKA S.G.R.N <124671914+ruchithx@users.noreply.github.com>
2024-07-10 19:56:00 +0530

    feat: add notfound page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -19,9 +19,15 @@ const config = {
     },
     extend: {
       boxShadow: {
-        'custom-shadow': '5px 10px 18px #888888',
+        "custom-shadow": "5px 10px 18px #888888",
+        custom: "5px 5px 10px #b9bbbb",
       },
       colors: {
+        "stv-yellow": "#f1c232",
+        "stv-blue": "#28a8e0",
+        "stv-dark-blue": "#0c2735",
+        "stv-green": "#1da518",
+        "stv-gray": "#f4f4f4",
         "custom-black": "#112735",
         "custom-light-blue": "#29A7E1",
         "custom-yellow": "#F1C332",
@@ -59,20 +65,19 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
+
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         animate: {
-          '0%': { opacity: '0' },
-          '50%': { opacity: '1', transform: 'rotate(45deg) translate(10px, 10px)' },
-          '100%': { opacity: '0' },
+          "0%": { opacity: "0" },
+          "50%": {
+            opacity: "1",
+            transform: "rotate(45deg) translate(10px, 10px)",
+          },
+          "100%": { opacity: "0" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
@@ -82,19 +87,20 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        
-          'animate': 'animate 2s infinite',
-        
+
+        animate: "animate 2s infinite",
       },
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        quicksand: ['Quicksand', 'sans-serif'],
-        roboto: ['Roboto', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
+        quicksand: ["Quicksand", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
       },
-      
+      spacing: {
+        "45": "45%",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
