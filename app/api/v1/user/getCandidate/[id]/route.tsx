6234c68de12d2b3e427d562cd293b1409f9d8c6e
MADHUSHANKA S.G.R.N <124671914+ruchithx@users.noreply.github.com>
2024-07-19 17:23:15 +0530

    feat: update changers

---
+++ app/api/v1/user/getCandidate/[id]/route.tsx
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { comparePassword } from "@/lib/auth";
+
+export async function GET(req: NextRequest, { params }: any) {
+  try {
+    const userId = params.id;
+    console.log(userId);
+    const candidate = await prisma.candidate.findFirst({
+      where: {
+        candidate_id: userId,
+      },
+      include: {
+        user: true, // Optionally include user details if needed
+      },
+    });
+    console.log(candidate);
+
+    if (!candidate) {
+      return NextResponse.json(
+        { message: "Candidate not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(candidate, { status: 200 });
+  } catch (e) {
+    console.log("💕💕💕" + e);
+    return NextResponse.json(
+      { message: "error of the server" },
+      { status: 500 }
+    );
+  }
+}
