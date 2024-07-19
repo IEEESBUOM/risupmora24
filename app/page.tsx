3ab01f0fb6460e6f1f9d26d80d70cdfb6b405691
ashandilz <ashandilsara8@gmail.com>
2024-07-19 15:26:19 +0530

    fix: homepage route

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import ComingSoon from "@/components/ComingSoon";
-import PanellistReg from "@/app/company/panelist-registration/[id]/page";
+
 import HomePage from "../components/HomePage";
 
 export default function Home() {
-  return <PanellistReg />;
-  // return<ComingSoon />
+  return <HomePage />;
 }
