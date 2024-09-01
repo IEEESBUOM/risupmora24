a719112115cc8dcbcd5d9b4600e1d5241fe298f0
MADHUSHANKA S.G.R.N <124671914+ruchithx@users.noreply.github.com>
2024-09-01 15:15:32 +0530

    feat: department cordinator and company coordinator route add

---
+++ app/api/v1/user/getUser/[id]/route.tsx
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { comparePassword } from "@/lib/auth";
+
+export async function GET(req: NextRequest, { params }: any) {
+  try {
+    const userId = params.id;
+    console.log(userId);
+    const candidate = await prisma.user.findFirst({
+      where: {
+        // user: {
+        id: userId, // Match the user ID with the id in the User table
+        // },
+      },
+      include: {
+        company_cordnator: {
+          include: {
+            company: true,
+          },
+        }, // Optionally include user details if needed
+        department_cordnator: true,
+      },
+    });
+    console.log(candidate);
+
+    if (!candidate) {
+      return NextResponse.json(
+        { message: "Candidate not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(candidate, { status: 200 });
+  } catch (e) {
+    return NextResponse.json(
+      { message: "error of the server" },
+      { status: 500 }
+    );
+  }
+}
---
+++ app/company/dashboard/[id]/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import StudentNavbar from "@/components/StudentNavbar";
+import ParticipantTable from "@/components/ParticipantTable";
+
+type Paramms = {
+  params: {
+    id: string;
+  };
+};
+
+const Page = ({ params }: Paramms) => {
+  // get panalist id from params
+  const panalistId = params.id;
+
+  // get candidate details by panalist id from allocation table in database, it means tou have to create the api , that allocation table panalist_id eqaul to gettting panalistId, then you will get the candidate id, then you have to get the candidate details by candidate id
+
+  // after that you have to pass the candidate details to the ParticipantTable component
+  // happy coding
+
+  return (
+    <div>
+      <div className="ml-20 mr-20">
+        <StudentNavbar />
+      </div>
+
+      <div className="container mx-auto p-4">
+        <ParticipantTable />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
