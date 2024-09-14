af92879cfe98c6284d3265d3ffc90211971a3622
ashandilz <ashandilsara8@gmail.com>
2024-09-14 20:42:43 +0530

    feat: added sensusHub for partners

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -12,6 +12,7 @@ import unilever from "../assets/img/unilever.png";
 import gtn from "../assets/img/gtn.png";
 import zero_beta from "../assets/img/zerobeta.png";
 import mas from "../assets/img/mas.png";
+import sensusHub from "../assets/img/SensusHub.jpeg";
 
 // gsap
 import { gsap } from "gsap";
@@ -122,20 +123,20 @@ function Partners() {
           </div>
         </div>
 
-        {/* <div className="row flex flex-wrap justify-center items-center mt-5">
+        <div className="row flex flex-wrap justify-center items-center mt-5">
           <div className="col-sm-4 flex flex-col items-center justify-center mb-8 sm:mb-0">
-            <div className="text-center text-lg w-64">Silver Partner</div>
+            <div className="text-center text-lg w-64">Bronze   Partner</div>
             <div className="flex justify-center items-center h-24">
               <Image
-                src={gtn}
-                alt="GTN"
-                width={150}
-                height={150}
+                src={sensusHub}
+                alt="sensusHub"
+                width={100}
+                height={100}
                 objectFit="contain"
               />
             </div>
           </div>
-          <div className="col-sm-4 flex flex-col items-center justify-center mb-8 sm:mb-0">
+          {/* <div className="col-sm-4 flex flex-col items-center justify-center mb-8 sm:mb-0">
             <div className="text-center text-lg w-64">Silver Partner</div>
             <div className="flex justify-center items-center h-24">
               <Image
@@ -149,8 +150,8 @@ function Partners() {
                 Coming Soon
               </div>
             </div>
-          </div>
-          <div className="col-sm-4 flex flex-col items-center justify-center mb-8 sm:mb-0">
+          </div> */}
+          {/* <div className="col-sm-4 flex flex-col items-center justify-center mb-8 sm:mb-0">
             <div className="text-center text-lg w-64">Bronze Partner</div>
             <div className="flex justify-center items-center h-24">
               <Image
@@ -164,8 +165,8 @@ function Partners() {
                 Coming Soon
               </div>
             </div>
-          </div>
-        </div> */}
+          </div> */}
+        </div>
       </div>
     </div>
   );
